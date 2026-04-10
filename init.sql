06129cf0ec1f3842c5f182b85ffe682edb262087
Eigmantas <eigmantaspeciulis@gmail.com>
2026-04-10 10:30:41 +0000

    aaa

--- init.sql
+++ init.sql
@@ -1,12 +1,16 @@
-CREATE TABLE IF NOT EXISTS snakers (
+CREATE TABLE IF NOT EXISTS sneakers  (
     id SERIAL PRIMARY KEY,
     model_id int,
-    color_id int,
+    color int,
     size int,
     image_url Varchar(255)
 );
 
-INSERT INTO snakers (model_id, color_id, size, image_url) VALUES
+TRUNCATE TABLE sneakers RESTART IDENTITY;
+
+INSERT INTO sneakers (model_id, color, size, image_url) VALUES
 (1, 1, 42, 'img/sneaker1.jpg'),
-(1, 2, 43, 'img/sneaker2.jpg'),
-(2, 1, 44, 'img/sneaker3.jpg');
+(2, 2, 43, 'img/sneaker2.jpg'),
+(3, 3, 44, 'img/sneaker3.jpg');
+
+
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src/views'));
 
-app.use(express.static(path.join(__dirname, 'src/public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', sneakerRoutes);
 
